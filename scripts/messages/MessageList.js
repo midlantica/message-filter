5d5d0fd5b364c0ed79b231fe522c23d2a3d13620
Andrew Harper <druharper@gmail.com>
2020-03-11 09:21:52 -0500

    init

---
+++ scripts/messages/MessageList.js
@@ -0,0 +1,34 @@
+import { Message } from "./Message.js"
+import { getMessagesByFriend } from "./MessageProvider.js"
+
+const contentTarget = document.querySelector(".messages")
+const friendListSection = document.querySelector(".friends")
+
+/*
+  COMPONENT FUNCTION
+*/
+export const MessageList = () => {
+  render([])
+}
+
+/*
+  RENDERING FUNCTION
+*/
+const render = messageArray => {
+  const convertedMessages = messageArray.map(messageObject => {
+    const messageHTML = Message(messageObject)
+    return messageHTML
+  })
+  const combinedSections = convertedMessages.join("")
+  contentTarget.innerHTML = combinedSections
+}
+
+// Listen for when a friend is selected
+friendListSection.addEventListener("change", changeEvent => {
+  if (changeEvent.target.classList.contains("friend")) {
+    // Get messages for friend and render the list of messages
+    const friendName = changeEvent.target.value
+    const messages = getMessagesByFriend(friendName)
+    render(messages)
+  }
+})
